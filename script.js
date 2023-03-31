8a0d9dded4ff321b12bd3d0ce026a2807bab10d9
Elijah Lawson <ElijahAtWork@gmail.com>
2023-03-31 12:23:02 -0500

    Added logic to parse input fields into an employee object and update the monthly counter at the bottom

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-console.log('IT WORKS');
+$(document).ready(onReady);
+
+function onReady() {
+    $('#submit-btn').on('click', addEmployee);
+}
+
+let employees = [];
+
+function addEmployee(event) {
+    event.preventDefault();
+
+    let employee = {};
+
+    employee.firstName = $('#first-name').val();
+    employee.lastName = $('#last-name').val();
+    employee.id = $('#id').val();
+    employee.title = $('#title').val();
+    unformattedSalary = $('#annual-salary').val();
+    employee.annualSalary = Number(unformattedSalary.replace(/[^0-9.-]+/g,""));
+
+    $('#first-name').val('')
+    $('#last-name').val('')
+    $('#id').val('')
+    $('#title').val('')
+    $('#annual-salary').val('')
+
+    $('tbody').append(`
+        <tr>
+            <td>${employee.firstName}</td>
+            <td>${employee.lastName}</td>
+            <td>${employee.id}</td>
+            <td>${employee.title}</td>
+            <td>${employee.annualSalary}</td>
+        </tr>
+    `);
+
+    employees.push(employee);
+
+    updateMonthly(employees);
+}
+
+function updateMonthly(employeesArr) {
+    total = 0;
+    const formatter = new Intl.NumberFormat('en-us', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+    });
+    
+    for (let employee of employeesArr) {
+        total += Math.round((Number(employee.annualSalary) / 12));
+    }
+
+    $('#monthly-amount').text(formatter.format(total))
+}
